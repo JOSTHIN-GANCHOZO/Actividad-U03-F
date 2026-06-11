df6a9fc43861ad39fea37ba8717c48d690c6caf4
JosthinGan <ganchozojosthin@gmail.com>
2026-06-10 22:38:50 -0500

    agregando rutas en el acrchivo de rutas de Equipos

---
+++ router/EquipoRouter.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import {
+            getAllEquipos,
+            getEquipoById,
+            crearEquipo
+        } from '../controllers/EquipoController.js';
+
+const router = Router();
+
+router.get('/equipos', getAllEquipos);
+router.get('/equipos/:id', getEquipoById);
+router.post('/equipos', crearEquipo);
+
+export default router;
